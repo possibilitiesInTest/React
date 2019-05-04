cf74f7b18e35bd722b8beb8a996a89c4aab4372d
Gourav B <pit.wire@pm.me>
2019-05-04 10:44:55 -0400

    create App component and import to index.js

---
+++ portfolio-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello!</h1>
+        <p>Hi my Name is Charlie. I'm a software engineer.</p>
+        <p>I'm always looking forward to working on meaningful projects.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
