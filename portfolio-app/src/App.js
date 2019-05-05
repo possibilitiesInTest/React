cfe68933062cc32793d4d8d310183d12e4c165fb
Gourav B <pit.wire@pm.me>
2019-05-05 09:37:45 -0400

    dynamically render data based on state

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from "react";
 
-class RegularClass {}
-class ComponentClass extends Component {}
+// class RegularClass {}
+// class ComponentClass extends Component {}
 
-const regularClassInstance = new RegularClass();
-const componentClassInstance = new ComponentClass();
+// const regularClassInstance = new RegularClass();
+// const componentClassInstance = new ComponentClass();
 
-console.log("regularClassInstance", regularClassInstance);
-console.log("componentClassInstance", componentClassInstance);
+// console.log("regularClassInstance", regularClassInstance);
+// console.log("componentClassInstance", componentClassInstance);
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { displayBio: true };
+  }
+
   render() {
+    const bio = this.state.displayBio ? (
+      <div>
+        <p>I live in San Francisco, and code every day.</p>
+        <p>
+          My favorite language is JavaScript, and I think React.js is awesome.
+        </p>
+        <p>Besides coding, I also love ramen and music.</p>
+      </div>
+    ) : null;
+
+    // if (!this.state.displayBio) {
+    //   bio = null;
+    // }
+
     return (
       <div>
         <h1>Hello!</h1>
         <p>Hi my Name is Charlie. I'm a software engineer.</p>
         <p>I'm always looking forward to working on meaningful projects.</p>
+        {bio}
       </div>
     );
   }
