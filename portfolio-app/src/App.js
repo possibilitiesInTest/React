c3947e965a9c2b36be56effc4d90619f003518a1
Gourav B <pit.wire@pm.me>
2019-05-06 14:04:13 -0400

    create SOCIAL_PROFILES[ ].  iterate through array w. map. binding.id as key. import to SocialProfiles. grab { link, image} and setTo local const. render array.params in SocialProfile class.

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Projects from "./Projects";
+import SocialProfiles from "./SocialProfiles";
 
 class App extends Component {
   state = { displayBio: false };
@@ -38,6 +39,8 @@ class App extends Component {
         )}
         <hr />
         <Projects />
+        <hr />
+        <SocialProfiles />
       </div>
     );
   }
