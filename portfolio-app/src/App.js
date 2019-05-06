d6a70665da499d1f9459cca6f5f2b0405985e6f4
Gourav B <pit.wire@pm.me>
2019-05-06 14:22:42 -0400

    add index.css and add styling

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Projects from "./Projects";
 import SocialProfiles from "./SocialProfiles";
+import profile from "./assets/profile.png";
 
 class App extends Component {
   state = { displayBio: false };
@@ -19,6 +20,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <img src={profile} alt="profile" className="profile" />
         <h1>Hello!</h1>
         <p>My name is David.</p>
         <p>I'm always looking forward to working on meaningful projects.</p>
