ed37493a04378037f4115a84efec15e2c128ff25
Gourav B <pit.wire@pm.me>
2019-05-10 07:54:31 -0400

    import react router. refactor app rendering with routes in index.js

--- portfolio-app/src/components/App.js
+++ portfolio-app/src/components/App.js
@@ -3,7 +3,6 @@ import Projects from "./Projects";
 import SocialProfiles from "./SocialProfiles";
 import profile from "../assets/profile.png";
 import Title from "./Title";
-import Jokes from "./Jokes";
 
 class App extends Component {
   state = { displayBio: false };
@@ -46,8 +45,6 @@ class App extends Component {
         <Projects />
         <hr />
         <SocialProfiles />
-        <hr />
-        <Jokes />
       </div>
     );
   }
--- portfolio-app/src/index.js
+++ portfolio-app/src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { Router, Switch, Route } from "react-router-dom";
+import createBrowserHistory from "history/createBrowserHistory";
 import App from "./components/App";
+import Jokes from "./components/Jokes";
 import "./index.css";
 
-ReactDom.render(<App />, document.getElementById("root"));
+ReactDom.render(
+  <Router history={createBrowserHistory()}>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/jokes" component={Jokes} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
