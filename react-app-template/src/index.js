c677562ddf07fa411a3333d66b3f28a8b8a18eef
Gourav Bhattacharyya <pit.wire@pm.me>
2019-05-12 08:54:47 -0400

    ----- ----- ----- ----- music-master-app starter files. create react-app-template

---
+++ react-app-template/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom";
+import { Router, Switch, Route } from "react-router-dom";
+import App from "./components/App";
+import "./index.css";
+
+ReactDom.render(<App />, document.getElementById("root")
+);
