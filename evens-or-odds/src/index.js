bcaf12dd8cba2caba9454d4a0d2497c95e3eab25
Gourav B <pit.wire@pm.me>
2019-05-16 17:55:32 -0400

    implement action creators - implement store w. reducer - implement action creators to set startGame and cancelGame - create const to replace string w. var for SET_GAME_STARTED - implement action creators for expanding and cancelling instructions - dispatch each action w. actions as input

--- evens-or-odds/src/index.js
+++ evens-or-odds/src/index.js
@@ -4,6 +4,9 @@ import { createStore } from "redux";
 import App from "./components/App";
 import "./index.css";
 
+const SET_GAME_STARTED = 'SET_GAME_STARTED';
+const SET_INSTRUCTIONS_EXPANDED = 'SET_INSTRUCTIONS_EXPANDED';
+
 const DEFAULT_SETTINGS = {
   gameStarted: false,
   instructionsExpanded: false
@@ -12,7 +15,7 @@ const DEFAULT_SETTINGS = {
 const rootReducer = (state = DEFAULT_SETTINGS, action) => {
   console.log('state', state, 'action', action);
 
-  if(action.type === 'SET_GAME_STARTED') {
+  if(action.type === SET_GAME_STARTED) {
     return {
       gameStarted: action.gameStarted,
       instructionsExpanded: false
@@ -22,16 +25,38 @@ const rootReducer = (state = DEFAULT_SETTINGS, action) => {
   return state;
 };
 
+// create store with reducer
 const store = createStore(rootReducer);
 console.log("store", store);
 console.log('store.getState()', store.getState());
 
+// log state whenever store is changed
 store.subscribe(() => console.log('store.getState()', store.getState()));
 
-const action1 = { gameStarted: true, type: 'SET_GAME_STARTED' };
-store.dispatch(action1);
 
-store.dispatch({ type: 'foo'});
-store.dispatch({ type: 'bar' });
+// action creators
+const startGame = () => {
+ return { type: SET_GAME_STARTED, gameStarted: true };
+}
+
+const cancelGame = () => {
+ return { type: SET_GAME_STARTED, gameStarted: false };
+}
+
+const expandInstructions =() => {
+  return {  type: SET_INSTRUCTIONS_EXPANDED, instructionsExpanded: true};
+}
+
+const cancelInstructions =() => {
+  return {  type: SET_INSTRUCTIONS_EXPANDED, instructionsExpanded: false};
+}
+
+// const action1 = startGame();
+// store.dispatch(action1);
+
+store.dispatch(startGame());
+store.dispatch(cancelGame());
+store.dispatch(expandInstructions());
+store.dispatch(cancelInstructions());
 
 ReactDom.render(<App />, document.getElementById("root"));
