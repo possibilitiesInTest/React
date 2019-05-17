9c9d3ae87f51606160b390dacd2777104fc80931
Gourav B <pit.wire@pm.me>
2019-05-17 14:52:58 -0400

    implement redux based UI: - add suit emojis to title - render ui based on game started/cancelled:   - dispatch start/cancel game action creators via onClick   - refactor calling functions directly in jsx:     - create local helpers for start/cancel game ( )s     - call helper methods in onClick

--- evens-or-odds/src/components/App.js
+++ evens-or-odds/src/components/App.js
@@ -1,23 +1,52 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
+import { startGame, cancelGame } from '../actions/settings';
 
 class App extends Component {
+    startGame = () => {
+        this.props.dispatch(startGame());
+    }
+
+    cancelGame = () => {
+        this.props.dispatch(cancelGame());
+    }
+
     render() {
         console.log('this', this);
 
         return (
-            <div>React App</div>
+            <div>
+                <h2> ♦ ♥ ️ Evens or Odds ♣ ♠</h2>
+                {
+                    this.props.gameStarted ? (
+                        <div>
+                            <h3>The game in on!</h3>
+                            <br />
+                            <button onClick={this.cancelGame}>Cancel Game</button>
+                        </div>
+                    ) : (
+                        <div>
+                            <h3>A new game awaits</h3>
+                            <br />
+                            <button onClick={ this.startGame }>Start Game</button>
+                        </div>
+                    )
+                }
+            </div>
         )
     }
 }
 
+
+// maps redux state to properties that get attached to component
 const mapStateToProps = state => {
     console.log('state', state);
 
     return { gameStarted: state.gameStarted };
 }
 
+// react-redux will call function to customize connector result
 const componentConnector = connect(mapStateToProps);
 
-
+// component now connected to redux store
 export default componentConnector(App);
