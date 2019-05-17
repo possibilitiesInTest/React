0ef249f866ff16a5bd9d9ef9555dec7c6ff3e0e9
Gourav B <pit.wire@pm.me>
2019-05-17 15:21:41 -0400

    map dispatch to props: - implement mapDispatchToProps, passing in dispatch as param     - return properties {startGame/cancelGame}     - which map to functions dispatching action creators through callbacks
    - pass mapDispatchToProps as second param in connect( )
    - rm local helper methods
    - refactor onClick methods to use actionCreators passed down through props

--- evens-or-odds/src/components/App.js
+++ evens-or-odds/src/components/App.js
@@ -3,13 +3,6 @@ import { connect } from 'react-redux';
 import { startGame, cancelGame } from '../actions/settings';
 
 class App extends Component {
-    startGame = () => {
-        this.props.dispatch(startGame());
-    }
-
-    cancelGame = () => {
-        this.props.dispatch(cancelGame());
-    }
 
     render() {
         console.log('this', this);
@@ -22,13 +15,13 @@ class App extends Component {
                         <div>
                             <h3>The game in on!</h3>
                             <br />
-                            <button onClick={this.cancelGame}>Cancel Game</button>
+                            <button onClick={this.props.cancelGame}>Cancel Game</button>
                         </div>
                     ) : (
                         <div>
                             <h3>A new game awaits</h3>
                             <br />
-                            <button onClick={ this.startGame }>Start Game</button>
+                            <button onClick={ this.props.startGame }>Start Game</button>
                         </div>
                     )
                 }
@@ -45,8 +38,16 @@ const mapStateToProps = state => {
     return { gameStarted: state.gameStarted };
 }
 
+// // allows attachment of action creator methods to props { }
+const mapDispatchToProps = dispatch => {
+    return {
+        startGame: () => dispatch(startGame()),
+        cancelGame: () => dispatch(cancelGame())
+    };
+}
+
 // react-redux will call function to customize connector result
-const componentConnector = connect(mapStateToProps);
+const componentConnector = connect(mapStateToProps, mapDispatchToProps);
 
 // component now connected to redux store
 export default componentConnector(App);
