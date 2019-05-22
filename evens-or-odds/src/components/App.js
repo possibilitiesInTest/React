2b4e29ea9e51f2bbd248cb5f124f3cc5c2e81296
Gourav B <pit.wire@pm.me>
2019-05-21 22:51:03 -0400

    track correct guesses:
    in gameState
         -set correctGuesses to zero in default game state
         -import DECK_DRAW
         -create case for DECK_DRAW.FETCH_SUCCESS
          deconstruct value from action.cards[0]
          deconstruct guess, correctGuesses from state
         -create an EVENS[ ]
         -create an ODDS[ ] starting w. ACE
         -check if guess is even && card is in evens
         -check if guess is odd && card is in odds
         -return ...state, correctGuesses+ keyed to cG
         -else return state for face card
    
    in components/GameState.js
         -import React, { connect }
         -init stateless functional GameState component
          passing in{remaining, correctguesses} deconstructed from (props)
         -export connector passing in GameState
         -in mSTP: destructure 'remaining' from deck,
                               'correctGuesses' from gameState
         -return <div> {remaining} cards remaining
                       {correctGuesses} correct guesses
         -dynamically render guess(es) w. ternary guessText const
         -replace 'guesses' w. {guessText}
    
    in App
         -import GameState
         -apply GameState element after title

--- evens-or-odds/src/components/App.js
+++ evens-or-odds/src/components/App.js
@@ -7,6 +7,7 @@ import Instructions from './Instructions';
 import DrawCard from './DrawCard';
 import Card from './Card';
 import Guess from './Guess';
+import GameState from './GameState';
 
 class App extends Component {
     startGame = () => {
@@ -34,6 +35,7 @@ class App extends Component {
                     this.props.gameStarted ? (
                         <div>
                             <h3>The game in on!</h3>
+                            <GameState />
                             <br />
                             <Guess />
                             <br/>
