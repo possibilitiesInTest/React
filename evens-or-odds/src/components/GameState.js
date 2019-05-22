88841a7e7a0f25904423e03ba6261fefc6894fc0
Gourav B <pit.wire@pm.me>
2019-05-21 23:22:24 -0400

    track correct guess record across games w. local storage:
    in components/GameState
         -init correctGuessesRecordKey='CORRECT_GUESSES_RECORD_foo123'
         -init checkrecord( ) inputting correctGuess
         -init record = correct..Key wrapped in Number( )
         //Number( ) sets returned nulls to 0
         -check if correctguesses > record, then setItem in localStorage
          to correctGuesses, return correctGuesses keyed to record,
          true returned for isNewRecord
         -return record, and false for isNewRecord
    
         -init recordObject = checkRecord passing in correctGuesses
         -destructure record, isnewrecord from recordObject
         -apply {record} value in gameState component
         -init recordLabel = New Record : Record
         -apply {recordLabel} to <div> alongside {record}

--- evens-or-odds/src/components/GameState.js
+++ evens-or-odds/src/components/GameState.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+const correctGuessesRecordKey = 'CORRECT_GUESSES_RECORD_foo123';
+
+const checkRecord = correctGuesses => {
+    const record = Number(localStorage.getItem(correctGuessesRecordKey));
+
+    if(correctGuesses > record) {
+        localStorage.setItem(correctGuessesRecordKey, correctGuesses);
+
+        return { record: correctGuesses, isNewRecord: true};
+    }
+    return { record, isNewRecord: false}
+};
+
 const GameState = ({ remaining, correctGuesses}) => {
-    const guessText = correctGuesses === 1 ? 'guess' : 'guesses'
+    const guessText = correctGuesses === 1 ? 'guess' : 'guesses';
+    const { record, isNewRecord } = checkRecord(correctGuesses);
+    const recordLabel  = isNewRecord ? 'New Record!' : 'Record';
 
     return (
         <div>
+            <p>{recordLabel}:{record}</p>
             <p>{remaining} cards remaining</p>
             <p>{correctGuesses} correct {guessText}.</p>
         </div>
     )
-}
+};
 
 export default connect(
     ({
