52679431c9ec4dfef7a3d6cf2094857d4febd2f5
unknown <pit.wire@pm.me>
2019-06-25 07:14:11 -0400

    writing create-react-app examples part I

---
+++ src/Gordon_Zac_React-Explained/Writing-Create-App-Examples/exercise-3/src/Hello.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Hello extends React.Component {
+    render() {
+        return <p className="Hello">
+            Hello!
+        </p>
+    }
+}
+
+
+export default Hello;
