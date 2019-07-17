8518a1bea451b6bbdf9d356fa773c5e89dae9322
Gourav B <pit.wire@pm.me>
2019-07-17 12:52:25 -0400

    seperating components && passing props

--- src/Girder-Stephen-React-w-Redux/components/src/index.js
+++ src/Girder-Stephen-React-w-Redux/components/src/index.js
@@ -1,60 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
 import faker from 'faker';
 
 const App = () => {
     return (
         <div className="ui container comments">
-        
-        {/* First Comment */}
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00pm</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
-
-        {/* Second Comment */}
-                    <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Jane
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00pm</span>
-                    </div>
-                    <div className="text">Great Read!</div>
-                </div>
-            </div>
-
-
-        {/* Third Comment */}
-        <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sigi
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00pm</span>
-                    </div>
-                    <div className="text">Keep on coding!</div>
-                </div>
-            </div>
-
+        <CommentDetail 
+            author="Samwise" 
+            timeAgo="Today at 4:45pm"
+            content="some text here" 
+            avatar={faker.image.avatar()} />
+        <CommentDetail 
+            author="Golem" 
+            timeAgo="Today at 2:00a"
+            content="lorem ipsum" 
+            avatar={faker.image.avatar()} />
+        <CommentDetail 
+            author="Frodo" 
+            timeAgo="Today at 3:15p"
+            content="dolorem amet" 
+            avatar={faker.image.avatar()} />
+        <CommentDetail 
+            author="Sauron" 
+            timeAgo="Yesterday at 10:33am"
+            content="is stere ruio" 
+            avatar={faker.image.avatar()} />
         </div>
     );
 };
