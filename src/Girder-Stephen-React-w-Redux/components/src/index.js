ab6fa39432321f20b62d730181d542f1bfb27b8e
Gourav B <pit.wire@pm.me>
2019-07-17 18:47:11 -0400

    passing one content component into another styled component

--- src/Girder-Stephen-React-w-Redux/components/src/index.js
+++ src/Girder-Stephen-React-w-Redux/components/src/index.js
@@ -1,31 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail';
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-        <CommentDetail 
-            author="Samwise" 
-            timeAgo="Today at 4:45pm"
-            content="some text here" 
-            avatar={faker.image.avatar()} />
-        <CommentDetail 
+            <ApprovalCard>
+                <CommentDetail 
+                author="Samwise" 
+                timeAgo="Today at 4:45pm"
+                content="some text here" 
+                avatar={faker.image.avatar()} 
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+            <CommentDetail 
             author="Golem" 
             timeAgo="Today at 2:00a"
             content="lorem ipsum" 
             avatar={faker.image.avatar()} />
-        <CommentDetail 
+
+            </ApprovalCard>
+        
+        <ApprovalCard>
+            <CommentDetail 
             author="Frodo" 
             timeAgo="Today at 3:15p"
             content="dolorem amet" 
             avatar={faker.image.avatar()} />
+        </ApprovalCard>
+
+        
+        <ApprovalCard>
         <CommentDetail 
             author="Sauron" 
             timeAgo="Yesterday at 10:33am"
             content="is stere ruio" 
             avatar={faker.image.avatar()} />
+        </ApprovalCard>
         </div>
     );
 };
