788027bcac3b9a3e836430fa0c0d4e28f2e90292
Gourav B <pit.wire@pm.me>
2019-07-19 07:37:29 -0400

    initialize App w. constructor():      -passing in props from super    -set lat to null in state    -interpolate lat in return    -call geolocation on App init

--- src/Girder-Stephen-React-w-Redux/seasons/src/index.js
+++ src/Girder-Stephen-React-w-Redux/seasons/src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null};
+        
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            err => console.log(err)       
+        );
+    }
+ 
     render() {
-    window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        err => console.log(err)       
-    );
-    return <div>Latitude: </div>
+    
+    return <div>Latitude: {this.state.lat}</div>
   }
 }
 
