b4635a432d9cc4341b078e68fff6ce322804d4f4
Gourav B <pit.wire@pm.me>
2019-07-21 20:55:39 -0400

    showing icons:
    SeasonDisplay
         -in SD: set icon to return snowflake|sun
         -add icon before and after h1
         -create seasonConfig {} w. season objects
          returning text, and iconName
         -remove ternarys for text and icon in SD
         -destructure text and iconName from seasonConfig[season]
         -replace icons w. iconNames in render

--- src/Girder-Stephen-React-w-Redux/seasons/src/SeasonDisplay.js
+++ src/Girder-Stephen-React-w-Redux/seasons/src/SeasonDisplay.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach!",
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Burr it is cold!',
+        iconName: 'snowflake'
+    }
+};
+
+
 const getSeason = (lat, month) => {
     if(month > 2 && month < 9) {
         return lat > 0 ? 'summer' : 'winter';
@@ -10,16 +22,17 @@ const getSeason = (lat, month) => {
 
 
 
-
 const SeasonDisplay = props => {
     const season = getSeason(props.lat, new Date().getMonth());
-    const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach!'
+    const { text, iconName } = seasonConfig[season];
 
     console.log(props.lat);
     console.log(season);
 
     return <div>
+        <i className={`${iconName} icon`} />
         <h1>{text}</h1>
+        <i className={`${iconName} icon`} />
         </div>;
 };
 
