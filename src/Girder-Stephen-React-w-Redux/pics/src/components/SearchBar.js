7d907372deedb72e8b44cf5d7dcd60c3eca9fff5
Gourav B <pit.wire@pm.me>
2019-07-22 15:23:52 -0400

    add event handler for search bar input:
    SearchBar
         -add onInputChange event passing in event
         -add onChange method to <input> passing in
          onInputChange as callback
    
         -default term to '' in state
         -in onChange set state from term to e.target.value
         -set val for input to term from state
    
         -add event handler to form tag passing in onFormSubmit
         -create onFormSubmit, preventing default refresh
         -refactor onFormSubmit w. => ( ) to prevent undefined err

--- src/Girder-Stephen-React-w-Redux/pics/src/components/SearchBar.js
+++ src/Girder-Stephen-React-w-Redux/pics/src/components/SearchBar.js
@@ -1,14 +1,28 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = { term: '' };
+
+    onFormSubmit = event => {
+        event.preventDefault();
+
+        console.log(this.state.term);
+    };
+
     render() {
-        return( 
-        <div className="ui segment">
-            <form className="ui form" style={{ marginTop: '10px' }}>
-                <label>Image Search</label>
-                <input type="text" />
-            </form>
-        </div>
+        return (
+            <div className="ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={e => this.setState({ term: e.target.value })}
+                            />
+                    </div>
+                </form>
+            </div>
         )
     }
 }
