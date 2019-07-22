39bb7f3298c14e828c1f473d55318fcaffdbcf1c
Gourav B <pit.wire@pm.me>
2019-07-21 22:30:22 -0400

    implement loading spinner:
    Spinner.js
         -create and export component
         -pass message from props to spinner
         -set defaultProps to loading message
    
    index.js
         -import spinner
         -replace loading text w. component
         -pass message from index
         -set default props messge to loading

--- src/Girder-Stephen-React-w-Redux/seasons/src/index.js
+++ src/Girder-Stephen-React-w-Redux/seasons/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
 
 class App extends React.Component {
     state = { lat: null, errMessage: ''};   
@@ -22,7 +24,7 @@ class App extends React.Component {
                 return <SeasonDisplay lat={this.state.lat}/>;
             }
 
-            return <div>Loading!</div>;
+            return <Spinner message="Please allow location access!" />;
         }
     }    
 
