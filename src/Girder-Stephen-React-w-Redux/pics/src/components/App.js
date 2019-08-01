bab7f61103717825b2241d4fee9072b4293822ef
gourav b <pit.wire@pm.me>
2019-08-01 00:21:56 -0400

    setting state after async request:
    App
         -set images to empty [] in state
         -set images to results of response data
          after completion of async
         -render number of images returned beneath
          searchBar component
         -implement arrow func on async to resolve
          misdirected 'this'

--- src/Girder-Stephen-React-w-Redux/pics/src/components/App.js
+++ src/Girder-Stephen-React-w-Redux/pics/src/components/App.js
@@ -4,7 +4,9 @@ import SearchBar from './SearchBar';
 
 
 class App extends React.Component {
-    async onSearchSubmit(term) {
+    state = { images: [] };
+
+    onSearchSubmit = async term => {
         const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term},
             headers: {
@@ -13,7 +15,7 @@ class App extends React.Component {
             }
         });
 
-        console.log(response.data.results);
+        this.setState({ images: response.data.results })
     }
 
 
@@ -21,6 +23,7 @@ class App extends React.Component {
     return ( 
     <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
     </div>
     );
     }
