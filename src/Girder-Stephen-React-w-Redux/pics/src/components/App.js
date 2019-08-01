36602e6d1d8acaa5d643cc15c24ec2f8964ef1db
gourav b <pit.wire@pm.me>
2019-07-31 23:59:44 -0400

    implement axios search to unsplash api:
    App
         -async onSearchSubmit
         -await get response
         -authorize client w. access key
         -console.log results from response data

--- src/Girder-Stephen-React-w-Redux/pics/src/components/App.js
+++ src/Girder-Stephen-React-w-Redux/pics/src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        console.log(term);
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term},
+            headers: {
+                Authorization: 
+                'Client-ID c67e1a5e3b5ab4e418d382a5f58bfa385daabd7067e0aab984ae4f6eb84340bd'
+            }
+        });
+
+        console.log(response.data.results);
     }
 
 
