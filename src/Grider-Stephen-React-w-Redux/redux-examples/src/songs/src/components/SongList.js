95fa99592d61868826e600b825888419873a3de6
gourav b <pit.wire@pm.me>
2019-08-09 09:06:05 -0400

    create SongDetail component:
    SongDetail
         -import React, { connect }
         -pass in song from props
         -render component if song
         -render song.title & .duration
         -pass selectedSong as song in mSTP
         -configure connect w. mSTP
    
    App
         -import render SongDetail
         -style w. semantic

--- src/Grider-Stephen-React-w-Redux/redux-examples/src/songs/src/components/SongList.js
+++ src/Grider-Stephen-React-w-Redux/redux-examples/src/songs/src/components/SongList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { selectSong } from 'actions';
+import { selectSong } from '../actions';
 
 class SongList extends Component {
     renderList() {
@@ -28,6 +28,7 @@ const mapStateToProps = (state) => {
     return { songs: state.songs};
 }
 
-export default connect(mapStateToProps, 
+export default connect(
+    mapStateToProps, 
     { selectSong }
 )(SongList);
