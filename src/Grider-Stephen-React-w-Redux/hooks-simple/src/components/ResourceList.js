35b4fb63f24c54a6d975792d0c21c8c7cc697095
Gourav B <pit.wire@pm.me>
2019-08-29 19:08:31 -0400

    implement custom hook for reusable logic:
    ResourceList
         -rf: resourceList as useResources, functional cmp,
          pass in resource, return resource
    
    useResources
         -import useState, useEffect, axios
         -cp.ovr useResources
         -export useRes as def
    
    UserList
          -import rest, useResources
         -imp: UserList as functional cmp
         -map ovr users returning id and name
         -exp.def UserList

--- src/Grider-Stephen-React-w-Redux/hooks-simple/src/components/ResourceList.js
+++ src/Grider-Stephen-React-w-Redux/hooks-simple/src/components/ResourceList.js
@@ -1,33 +1,13 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
+import useResources from "./useResources";
 
 const ResourceList = ({ resource }) => {
-  const [resources, setResources] = useState([]);
-
-  useEffect(() => {
-    (async resource => {
-      const response = await axios.get(
-        `https://jsonplaceholder.typicode.com/${this.props.resource}`
-      );
-      setResources(response.data);
-    })(resource);
-  }, [resource]);
-
-  // useEffect gets called every time component gets rendered
-
-  // async componentDidUpdate(prevProps) {
-  //   if (prevProps.resource !== this.props.resource) {
-  //     const response = await axios.get(
-  //       `https://jsonplaceholder.typicode.com/${this.props.resource}`
-  //     );
-  //     this.setState({ resources: response.data });
-  //   }
-  // }
+  const resources = useResources(resource);
 
   return (
     <div>
       {resources.map(record => (
-        <li key={record.id}></li>
+        <li key={record.id}>{record.title}</li>
       ))}
     </div>
   );
