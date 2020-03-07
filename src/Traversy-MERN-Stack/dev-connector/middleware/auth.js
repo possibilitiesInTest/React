a5ef71603ccaff670ba1a3ca846f82253f70360e
pit-dev <possibilitiesInTest@protonmail.com>
2020-03-07 17:17:39 -0500

    create, update routes via findeOne by user.id and  profile fields

--- src/Traversy-MERN-Stack/dev-connector/middleware/auth.js
+++ src/Traversy-MERN-Stack/dev-connector/middleware/auth.js
@@ -12,11 +12,20 @@ module.exports = function(req, res, next) {
 
   // verify token
   try {
-    const decoded = jwt.verify(token, config.get("jwtSecret"));
-
-    req.user = decoded.user;
-    next();
+    const decoded = jwt.verify(
+      token,
+      config.get("jwtSecret"),
+      (error, decoded) => {
+        if (error) {
+          res.status(401).json({ msg: "Token is not valid" });
+        } else {
+          req.user = decoded.user;
+          next();
+        }
+      }
+    );
   } catch (err) {
-    res.status(401).json({ msg: "token is not valid" });
+    console.error("something wrong with auth middleware");
+    res.status(500).json({ msg: "Server Error" });
   }
 };
