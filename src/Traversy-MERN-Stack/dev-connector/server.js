022cf07f046643b9f13a0b52a40caf3416a041a5
Gourav B <pit.wire@pm.me>
2020-04-21 03:59:05 -0400

    restart project: simple server setup, init deps install

--- src/Traversy-MERN-Stack/dev-connector/server.js
+++ src/Traversy-MERN-Stack/dev-connector/server.js
@@ -1,22 +1,9 @@
-const express = require("express");
-const connectDB = require("./config/db");
+const express = require('express');
 
 const app = express();
 
-// Connect Database
-connectDB();
+app.get('/', (req, res) => res.send('API Running'));
 
-// Init Middleware
-app.use(express.json({ extended: false }));
-
-app.get("/", (req, res) => res.send("API Running"));
-
-// Define routes
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/profile", require("./routes/api/profile"));
-app.use("/api/posts", require("./routes/api/posts"));
-
-const PORT = process.emitWarning.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
