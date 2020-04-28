74d5c709817fa882944a574c665ba6c44e142a2c
Gourav B <pit.wire@pm.me>
2020-04-27 23:19:15 -0400

    create user model, user reg route, apply exp-validator

--- src/Traversy-MERN-Stack/dev-connector/routes/api/users.js
+++ src/Traversy-MERN-Stack/dev-connector/routes/api/users.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 // #route GET api/users
 // @desc  Test route
 // @access Public
 
-router.get('/', (req, res) => res.send('Users route'));
+// router.get('/', (req, res) => res.send('Users route'));
+
+router.post(
+    "/",
+    [
+      check("name", "Name is requried")
+        .not()
+        .isEmpty(),
+      check("email", "Please include a valid email").isEmail(),
+      check("password", "Password must be 6 or more characters").isLength({
+        min: 6
+      })
+    ],
+    (req, res) => {
+      console.log(req.body);
+      const errors = validationResult(req);
+  
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array()
+        });
+      }
+  
+      res.send("User registered ...");
+    }
+  );
 
 module.exports = router;
