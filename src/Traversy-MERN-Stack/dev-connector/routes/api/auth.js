b7ffd89a802857b34a5decc6b4c8954001686c7d
Gourav B <pit.wire@pm.me>
2020-04-30 16:00:58 -0400

    implement auth middleware in signin route check if user exists compare credentials verify token auth

--- src/Traversy-MERN-Stack/dev-connector/routes/api/auth.js
+++ src/Traversy-MERN-Stack/dev-connector/routes/api/auth.js
@@ -17,4 +17,72 @@ router.get("/", auth, async (req, res) => {
     }
   });
 
+// READ existing USER route: SIGNIN
+// post req to api/auth
+//      passing: email, password,
+//      err. invalid creds, user/pw,
+//      valid user/pw, returns token
+router.post(
+    "/",
+    [
+      check("email", "Please include a valid email").isEmail(),
+      check("password", "Password is required").exists()
+    ],
+    async (req, res) => {
+      console.log(req.body);
+      const errors = validationResult(req);
+  
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array()
+        });
+      }
+  
+      const { email, password } = req.body;
+  
+      try {
+        // see if user exists
+        let user = await User.findOne({ email });
+  
+        // if user has not recognized, err: invalid creds
+        if (!user) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: "Invalid Credentials" }] });
+        }
+  
+        // compare user credentials
+        const isMatch = await bcrypt.compare(password, user.password);
+  
+        if (!isMatch) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: "Invalid Credentials" }] });
+        }
+  
+        // return jsonwebtoken
+        const payload = {
+          user: {
+            id: user.id
+          }
+        };
+  
+        jwt.sign(
+          payload,
+          config.get("jwtSecret"),
+          { expiresIn: 3600 },
+          (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+          }
+        );
+  
+        // res.send("User registered ...");
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error");
+      }
+    }
+  );
+
 module.exports = router;
