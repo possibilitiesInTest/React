6c1e1094eefbb1d8ed1a30bc111c9ac9e5207bfc
Gourav B <pit.wire@pm.me>
2020-05-06 18:58:18 -0400

    create get posts, byId, and delete route

--- src/Traversy-MERN-Stack/dev-connector/routes/api/posts.js
+++ src/Traversy-MERN-Stack/dev-connector/routes/api/posts.js
@@ -12,7 +12,6 @@ const User = require('../../models/User');
 // #route POST api/users
 // @desc  Test route
 // @access Private
-
 router.post(
     '/',
      [
@@ -49,5 +48,69 @@ router.post(
     }
  }
 );
+
+// #route GET api/users
+// @desc  Get all posts
+// @access Private
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.findById(req.params.id).sort({ date: -1 });
+        res.json(posts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// #route GET api/posts
+// @desc  Get posts by ID
+// @access Private
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // check if post w. id exists
+        if(!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        }        
+        console.log("Post Found and returned...");
+        res.json(post);
+
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
+// #route DELETE api/posts/:id
+// @desc Delete a post
+// @access Private
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if(!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        // Check user
+        if(post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized' });
+        }
+        await post.remove();
+        console.log("Post removed ...");
+        res.json({ msg: 'Post removed' });
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
+
 
 module.exports = router;
