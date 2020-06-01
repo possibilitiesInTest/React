a29a85ab6241c4f10fbe0658d9102905b61bd9f4
Gourav B <pit.wire@pm.me>
2020-05-31 21:32:18 -0400

    setup redux store

--- src/Traversy-MERN-Stack/dev-connector/client/src/App.js
+++ src/Traversy-MERN-Stack/dev-connector/client/src/App.js
@@ -5,10 +5,14 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import './App.css';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store'; 
 
-const App = () =>
+const App = () => (
+  <Provider store={store}>
+  <Router>
     <Fragment>
-      <Router>
       <Navbar />
       <Route exact path="/" component={Landing} />
       <section className="container">
@@ -17,8 +21,8 @@ const App = () =>
           <Route exact path="/login" component={Login} />
         </Switch>
       </section>
-      </Router>n>
     </Fragment>
-
-
+    </Router>
+    </Provider>
+);
 export default App;
