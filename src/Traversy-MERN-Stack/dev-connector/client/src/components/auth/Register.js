2717a149752e5c0bddad55331873c0461eb04bf3
pit-dev <possibilitiesInTest@protonmail.com>
2020-07-04 03:28:03 -0400

    create alert component in layout and action call

--- src/Traversy-MERN-Stack/dev-connector/client/src/components/auth/Register.js
+++ src/Traversy-MERN-Stack/dev-connector/client/src/components/auth/Register.js
@@ -1,42 +1,54 @@
-import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
 
-const Register = () => {
-    const[formData, setFormData] = useState({
-        name: '',
-        email: '',
-        password: '',
-        password2: ''
-    });
+const Register = ({ setAlert }) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
 
-    const { name, email, password, password2 } = formData;
+  const { name, email, password, password2 } = formData;
 
-    const onChange = e => 
-        setFormData({...formData, [e.target.name]: e.target.value });
-    
-    const onSubmit = async e => {
-        e.preventDefault();
-        if(password !== password2){
-            console.log('Passwords do not match');
-        } else {
-            console.log("Success");
-        }
-    };
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
-    return <Fragment>
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      setAlert("Passwords do not match", danger);
+    } else {
+      console.log("Success");
+    }
+  };
+
+  return (
+    <Fragment>
       <h1 className="large text-primary">Sign Up</h1>
-      <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
-      <form className="form" onSubmit={e => onSubmit(e)}>
+      <p className="lead">
+        <i className="fas fa-user"></i> Create Your Account
+      </p>
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
         <div className="form-group">
-          <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
-
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={(e) => onChange(e)}
+            required
+          />
         </div>
         <div className="form-group">
           <input type="email" placeholder="Email Address" name="email" />
-          <small className="form-text"
-            >This site uses Gravatar so if you want a profile image, use a
-            Gravatar email</small
-          >
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
         </div>
         <div className="form-group">
           <input
@@ -44,7 +56,7 @@ const Register = () => {
             placeholder="Password"
             name="password"
             value={password}
-            onChange={e => onChange(e)}
+            onChange={(e) => onChange(e)}
             minLength="6"
           />
         </div>
@@ -54,7 +66,7 @@ const Register = () => {
             placeholder="Confirm Password"
             name="password2"
             value={password2}
-            onChange={e => onChange(e)}
+            onChange={(e) => onChange(e)}
             minLength="6"
           />
         </div>
@@ -64,6 +76,11 @@ const Register = () => {
         Already have an account? <Link To="/login">Sign In</Link>
       </p>
     </Fragment>
-}
+  );
+};
 
-export default Register
+Register.propTyoes = {
+  setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert })(Register);
