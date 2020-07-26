6fd5a1411bf06b3f0341da8f9bb02d378376e9d8
pit-dev <possibilitiesInTest@protonmail.com>
2020-07-25 23:20:59 -0400

    add LOGIN_SUCCESS|FAIL to action types, auth reducer cases
    pass isAUthenticated through mSTP in Login and Register cmps

--- src/Traversy-MERN-Stack/dev-connector/client/src/actions/auth.js
+++ src/Traversy-MERN-Stack/dev-connector/client/src/actions/auth.js
@@ -6,7 +6,7 @@ import {
   USER_LOADED,
   AUTH_ERROR,
 } from "./types";
-import setAuthToken from 
+import setAuthToken from "../utils/setAuthToken";
 
 // LOAD USER
 export const loadUser = () => async (dispatch) => {
@@ -54,6 +54,36 @@ export const register = ({ name, email, password }) => async (dispatch) => {
 
     dispatch({
       type: REGISTER_FAIL,
+    });
+  }
+};
+
+// LOGIN USER
+export const login = ({ email, password }) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post("/api/auth", body, config);
+
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data,
+    });
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+    }
+
+    dispatch({
+      type: LOGIN_FAIL,
     });
   }
 };
