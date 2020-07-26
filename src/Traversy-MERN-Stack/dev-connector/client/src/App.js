a1dcdc8b72ef27acf117945669a9dcbc6b440c88
pit-dev <possibilitiesInTest@protonmail.com>
2020-07-25 22:27:13 -0400

    create actions|reducer cases for      -REGISTER_SUCCESS      -REGISTER_FAIL      -USER_LOADED      -AUTH_ERROR
    -check for token in main App
    -add useEffect hook to loadUser w.ever App is loaded

--- src/Traversy-MERN-Stack/dev-connector/client/src/App.js
+++ src/Traversy-MERN-Stack/dev-connector/client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
@@ -9,21 +9,33 @@ import "./App.css";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
+import { loadUser } from "./actions.auth";
+import setAuthToken from "./reducers/utils";
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <section className="container">
-          <Switch>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 export default App;
--- src/Traversy-MERN-Stack/dev-connector/client/src/actions/types.js
+++ src/Traversy-MERN-Stack/dev-connector/client/src/actions/types.js
@@ -1,1 +1,6 @@
-export const SET_ALERT='SET_ALERT';
+export const SET_ALERT = "SET_ALERT";
+export const REMOVE_ALERT = "REMOVE_ALERT";
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
+export const USER_LOADED = "USER_LOADED";
+export const AUH_ERROR = "AUTH_ERROR";
