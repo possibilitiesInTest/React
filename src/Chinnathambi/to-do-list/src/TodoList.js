69e2e5a6fbe2158716ffb7c14059365edc7000a7
pit-dev <possibilitiesInTest@protonmail.com>
2020-08-06 14:40:31 -0400

    extract this._inputElement, addItem via itemArray.unshift

---
+++ src/Chinnathambi/to-do-list/src/TodoList.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      item: [],
+    };
+
+    this.addItem = this.addItem.bind(this);
+  }
+  addItem(e) {
+    var itemArray = this.state.items;
+
+    if (this._inputElement.value !== "") {
+      itemArray.unshift({
+        text: this._inputElement.value,
+        key: Date.now(),
+      });
+
+      this.setState({
+        items: itemArray,
+      });
+
+      this._inputElement.value = "";
+    }
+    console.log(itemArray);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="todoListMain">
+        <div className="header">
+          <form onSubmit={this.addItem}>
+            <input
+              ref={(a) => (this._inputElement = a)}
+              placeholder="enter task"
+            ></input>
+            <button type="submit">add</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
