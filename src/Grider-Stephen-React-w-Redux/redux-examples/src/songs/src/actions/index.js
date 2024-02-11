4a35cb92151c7dd4bcafe6e51a22a1fc4bc8c887
possibilitiesInTest <possibilitiesInTest@gmail.com>
2024-02-11 17:54:27 -0500

    clean repo

--- src/Grider-Stephen-React-w-Redux/redux-examples/src/songs/src/actions/index.js
+++
@@ -1,8 +0,0 @@
-// Action creator
-const selectSong = () => {
-    // Return an action
-    return {
-        type: 'SONG_SELECTED',
-        payload: song
-    };
-};
--- src/Katz_David_Joseph_ReactJS-and-Redux-Bootcamp/music-master-app/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import { Router, Switch, Route } from "react-router-dom";
-import App from "./components/App";
-import "./index.css";
-
-ReactDom.render(<App />, document.getElementById("root")
-);
